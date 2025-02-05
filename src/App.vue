f743a373c2951ba457863600aab0d907daba5dcb
jhocoder <jhonathanrugeb@gmail.com>
2025-02-05 12:30:33 +0100

    add project with firebase and credentianls

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header class="">
+
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
